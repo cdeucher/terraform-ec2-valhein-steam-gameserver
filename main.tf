414e5b4eae0bcfed1e8684eaabc3799386475800
ryanface <cristianoboell@yahoo.com.br>
2021-03-06 18:30:10 -0300

    fixed  remote-exec to setup instance

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "web-server-instance" {
   ami               = "ami-02fe94dee086c0c37"
   instance_type     = "t2.medium"
   availability_zone = "us-east-1a"
-  key_name          = "private_key" 
+  key_name          = "Ansible_local" 
   # This EC2 Instance has a public IP and will be accessible directly from the public Internet
   #associate_public_ip_address = true
 
@@ -26,46 +26,52 @@ resource "aws_instance" "web-server-instance" {
       network_interface_id = aws_network_interface.web-server-io.id
   }
 
-  /*connection {
-        # The default username for our AMI
-        user = "ubuntu"
-        host = aws_instance.web-server-instance.public_ip
-        type = "ssh"
-        private_key = "${file("private_key.pem")}"
+  tags = {
+      Name = "Production-instance"
+  }
+}
+
+resource "null_resource" "bash_remote" {
+  connection {
+      private_key = file("MyKeyPair.pem")
+      user        = "ubuntu"
+      host        = aws_instance.web-server-instance.public_ip
   }
 
   provisioner "remote-exec" {
+    inline = ["echo 'connected!'"]
+  }
+  provisioner "remote-exec" {
     inline = [
-          "sudo apt update -y",
-          "sudo dpkg --add-architecture i386",
-          "sudo apt -y update",
-          "sudo mkdir -p /data/lgsm/data",
-          "sudo chmod -R 777 /data",
-          "sudo apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6",
-          "sudo apt install -y libsdl2-2.0-0:i386"
+                "sudo apt-get -qq -y update"
+                ,"sudo dpkg --add-architecture i386" 
+                ,"sudo apt-get -qq -y update"  
+                ,"sudo mkdir /data" 
+                ,"sudo chmod -R 777 /data" 
+                ,"sudo apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6"  
+                ,"sudo apt-get -qq -y install libsdl2-2.0-0:i386"
+                ,"sudo mount /dev/xvdd /data"
+                ,"mkdir -p ~/.config/unity3d/IronGate/Valheim/worlds"
+                ,"sudo rsync -avuz /data/world_dir/* ~/.config/unity3d/IronGate/Valheim/worlds/"
+                ,"sudo cat '*/5 * * * * /data/save_worlds.sh' > /etc/cron.d/custom"
+                ,"sudo sudo /etc/init.d/cron restart"
+                #,"sudo apt-get -qq -y install steamcmd"
+                #,"cd /data && ./vhserver start"                                
     ]
-  }*/
-   
-  //user_data = "${file("data/index.php")}"
-  user_data  = <<-EOF
-                #!/bin/bash
-                sudo apt update -y
-                sudo dpkg --add-architecture i386
-                sudo apt -y update
-                sudo chmod -R 777 /data
-                sudo apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 
-                sudo apt install -y libsdl2-2.0-0:i386
-                #sudo apt -y install steamcmd
-                sudo mount /dev/xvdd /data
-                
-                #cd /data && ./vhserver start
-                
-
+  }   
+                 
+  /*
+  provisioner "local-exec" {
+    command = <<-EOF
                 #
                 # SETUP VOLUME (only new volumes)
-                #
+
+                # vi save_worlds.sh
+                # #!/bin/bash
+                # rsync -avuz ~/.config/unity3d/IronGate/Valheim/worlds /data/world_dir 
+                
                 #ls -la ~/.config/unity3d/IronGate/Valheim/worlds/
-                #*/5 * * * * /data/save_worlds.sh > /dev/null 2>&1
+                #5 * * * * /data/save_worlds.sh > /dev/null 2>&1
                 #sudo mkfs -t xfs /dev/xvdd
                 #sudo mkdir -p /data/lgsm/data
                 #sudo apt -y install steamcmd
@@ -74,13 +80,13 @@ resource "aws_instance" "web-server-instance" {
                 #bash linuxgsm.sh vhserver
                 #./vhserver install
                 #./vhserver start
-    
                 EOF
-
-  tags = {
-      Name = "Production-instance"
-  }
+  } 
+  */
+  depends_on  = [aws_instance.web-server-instance, aws_volume_attachment.ebs_att]    
 }
+
+
 output "base_ip" {
   value = aws_instance.web-server-instance.public_ip
 }
